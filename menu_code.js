9936e9a4ba86a57522b4a63eeec0f13f4917e799
Lukáš Vais <vais.luk@gmail.com>
2021-02-06 23:48:58 +0100

    Discard all tabs in/except current window

--- menu_code.js
+++ menu_code.js
@@ -1,14 +1,38 @@
-discardAllOtherTabs.addEventListener("click", async () => {
+buttonDiscardAllTabsInAllWindows.addEventListener("click", async () => {
   discardAllTabsInAllWindows();
 });
 
+buttonDiscardAllTabsInCurrentWindow.addEventListener("click", async () => {
+  discardAllTabsInCurrentWindow();
+});
+
+buttonDiscardAllTabsExceptInCurrentWindow.addEventListener("click", async () => {
+  discardAllTabsExceptInCurrentWindow();
+});
+
 function discardAllTabsInAllWindows() {
-  chrome.tabs.query({}, function (tabs) {
-    for (const tab of tabs) {
-      chrome.tabs.discard(tab.id, (discardedTab) => {
-        if (discardedTab) {
-        }
-      });
-    }
-  });
+  chrome.tabs.query({}, discardTabs);
+}
+
+function discardAllTabsInCurrentWindow() {
+  chrome.tabs.query({ currentWindow: true }, discardTabs);
+}
+
+function discardAllTabsExceptInCurrentWindow() {
+  chrome.tabs.query({ currentWindow: false }, discardTabs);
+}
+
+function discardTabs(tabs) {
+  console.log("Discarding tabs: ", tabs);
+
+  for (const tab of tabs) {
+    chrome.tabs.discard(tab.id, (discardedTab) => {
+      if (chrome.runtime.lastError) {
+        // console.warn("Whoops.. " + chrome.runtime.lastError.message);
+      }
+
+      if (discardedTab) {
+      }
+    });
+  }
 }
